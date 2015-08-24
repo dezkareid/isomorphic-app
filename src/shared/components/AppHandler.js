799ed877564b40a3d85a9a09a1374ec2320c962b
Joel Humberto Gómez Paredes <elmaildeldezkareid@gmail.com>
2015-08-24 00:08:55 -0500

    Added initial configuration

---
+++ src/shared/components/AppHandler.js
@@ -0,0 +1,8 @@
+import React from "react";
+import jade from 'react-jade';
+
+export default class AppHandler extends React.Component {  
+  render() {
+    return <div>Hello App Handler</div>;
+  }
+}
